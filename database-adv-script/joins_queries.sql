e44777b2eb4210fe270218a39d9038f892fff95b
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2025-05-13 22:46:20 +0100

    fix(joins_queries): correct LEFT JOIN query to include ORDER BY clause

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -10,7 +10,8 @@ INNER JOIN "user" ON booking.user_id = "user".user_id;
 -- LEFT JOIN
 -- This Quesrt retrieves all properties and their reviws including those without reviews
 SELECT * FROM property
-LEFT JOIN review ON property.property_id = review.property_id;
+LEFT JOIN review ON property.property_id = review.property_id
+ORDER BY property.property_id;
 
 -- FULL OUTER JOIN
 -- This Quesrt retrieves all users and their booking including those without bookings
