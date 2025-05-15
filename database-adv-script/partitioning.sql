af539e3b1d5b26d2c0f0baa0a50bae876bb8d42c
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2025-05-15 11:14:55 +0100

    feat(database-adv-script): enhance partitioning script for booking table with improved data backup and restoration process

--- database-adv-script/partitioning.sql
+++ database-adv-script/partitioning.sql
@@ -6,20 +6,35 @@
 EXPLAIN ANALYZE
 SELECT * FROM booking WHERE start_date BETWEEN '2024-06-01' AND '2024-06-30';
 
+-- backup data fo booking table
+ALTER TABLE booking RENAME TO booking_old;
 
--- Create partitions for the 'booking' table by year
--- Partition for bookings in 2024
+-- Create the partitioned parent table with PARTITION BY
+CREATE TABLE booking (
+    booking_id UUID NOT NULL,
+    property_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_price DECIMAL NOT NULL,
+    status VARCHAR CHECK (status IN ('pending', 'confirmed', 'canceled')) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (booking_id, start_date)
+) PARTITION BY RANGE (start_date);
+
+-- restore data from booking_old to partitioned booking table
+INSERT INTO booking (booking_id, property_id, user_id, start_date, end_date, total_price, status, created_at)
+
+
+-- Create partitions for each year
 CREATE TABLE booking_2024 PARTITION OF booking
     FOR VALUES FROM ('2024-01-01') TO ('2025-01-01');
 
--- Partition for bookings in 2025
 CREATE TABLE booking_2025 PARTITION OF booking
     FOR VALUES FROM ('2025-01-01') TO ('2026-01-01');
 
--- Default partition for bookings from 2026 onwards
 CREATE TABLE booking_future PARTITION OF booking
     FOR VALUES FROM ('2026-01-01') TO ('2100-01-01');
-
 
 -- Analyze query performance after partitioning
 EXPLAIN ANALYZE
